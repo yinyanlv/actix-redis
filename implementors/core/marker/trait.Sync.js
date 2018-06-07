d51f6efb61a4c08a258593aca2a5a2fa5b237f02
Travis CI User <travis@example.org>
2018-06-07 16:16:44 +0000

    Documentation upload

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_redis"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"actix_redis/struct.Command.html\" title=\"struct actix_redis::Command\">Command</a>",synthetic:true,types:["actix_redis::redis::Command"]},{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"actix_redis/struct.RedisActor.html\" title=\"struct actix_redis::RedisActor\">RedisActor</a>",synthetic:true,types:["actix_redis::redis::RedisActor"]},{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"actix_redis/struct.RedisSessionBackend.html\" title=\"struct actix_redis::RedisSessionBackend\">RedisSessionBackend</a>",synthetic:true,types:["actix_redis::session::RedisSessionBackend"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"actix_redis/enum.Error.html\" title=\"enum actix_redis::Error\">Error</a>",synthetic:false,types:["actix_redis::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
